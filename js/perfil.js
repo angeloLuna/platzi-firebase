390510414f6a3ca93ce02a559c8615a2920311fb
angeloLuna <angel.luna.najera@gmail.com>
2018-08-06 18:28:59 -0500

    Agregada interaccion al formulario de perfil

--- js/perfil.js
+++ js/perfil.js
@@ -3,14 +3,25 @@ var ref = firebase.database().ref('usuario');
 var btnLogin = document.getElementById("btnLogin");
 var btnLogout = document.getElementById("btnLogout");
 
+var datosPerfil = document.getElementById("datosPerfil");
+var formularioPerfil = document.getElementById("formularioPerfil");
 var perfilNombre = document.getElementById("perfilNombre");
 var perfilEmail = document.getElementById("perfilEmail");
 
+var cancelForm = document.getElementById("cancelForm");
+var nombreForm = document.getElementById("nombreForm");
+var telefonoForm = document.getElementById("telefonoForm");
+var calleForm = document.getElementById("calleForm");
+var interiorForm = document.getElementById("interiorForm");
+var coloniaForm = document.getElementById("coloniaForm");
+var cpForm = document.getElementById("cpForm");
+
+var btnEditar = document.getElementById("perfilEditar");
+
 var usuario = {}
 
-function leerInformacion(){
-  // debemos cambiar el uid por uno válido 
-  ref.child("0Z6jPxp55raG0y9ruAVYQbm7NHp2").on("value", function(data){
+function leerInformacion(uid){
+  ref.child(uid).on("value", function(data){
     console.log(data.val())
     llenarInformacion(data.val().nombre, data.val().email)
   })
@@ -20,13 +31,13 @@ function llenarInformacion(nombre, email){
   perfilNombre.innerHTML = nombre;
   perfilEmail.innerHTML = email
 }
-leerInformacion()
 
 
 firebase.auth().onAuthStateChanged(function(user){
   console.log(user)
   if (user) {
     console.log("tenemos usuario");
+    leerInformacion(user.uid);
     mostrarLogout()
   }else{
     window.location.href = "index.html"
@@ -51,4 +62,20 @@ function mostrarLogin(){
   btnLogout.style.display = "none";
   btnLogin.style.display = "block";
 }
+
+perfilEditar.addEventListener("click", function(){
+  datosPerfil.style.display = "none";
+  formularioPerfil.style.display = "block";
+});
+
+cancelForm.addEventListener("click", function(){
+  datosPerfil.style.display = "block";
+  formularioPerfil.style.display = "none";
+})
+
+function editarDatos(){
+  event.preventDefault()
+  console.log("editar datos")
+}
+
 
